e3bf1ee6b37b03127b5bf7c442af1036b00771f3
qiong <vcrting@163.com>
2023-03-20 12:10:47 +0800

    0

---
+++ Logistics/dist/assets/shop_iist.8f2a5e86.js
@@ -0,0 +1,1 @@
+import{d as e,v as o,o as t,c as n}from"./index.81274d1c.js";const _=e({__name:"shop_iist",setup(s){return o(null),(a,r)=>(t(),n("div"))}});export{_ as default};
--- Logistics/src/air/app/pagin.ts
+++ Logistics/src/air/app/pagin.ts
@@ -1,27 +1,33 @@
 
 // 赋值
-const ser_pagi_more = (iist: MANY): ONE => {
+const ser_pagi_more = (iist: MANY, _num: number): ONE => {
     let res = { data: <MANY>[ ], page: <ONE>{ total: 0 } }
-    iist.map((e: ONE) => { res.data.push( ...e.data ); res.page.total = e.page.total }); return res
+    iist.map((e: ONE) => { 
+        res.data.push( ...e.data ); 
+        if (e.page.total) {
+            res.page.total = e.page.total 
+            res.page._total = Math.abs( Math.ceil( e.page.total / _num ) )
+        }
+    }); 
+    return res
 }
 
 // 获取页面 数量
 const _got_pages = (_num: number, star: number): number[ ] => {
-    const __ends = [ ]
-    for (let i= 1; i<= _num; i++ ) { const _st = i + (_num * (star - 1) ); __ends.push(_st) } return __ends
+    const __ends = [ ]; for (let i= 1; i<= _num; i++ ) { const _st = i + star; __ends.push(_st) } return __ends
 }
 
 export default {
     // 此方法
     pagin_more: async (prm: ONE, caii: Function, _num: number = 5, _iong: number = 100): Promise<ONE> => {
         return new Promise(async (rej) => {
-            prm['pagination[page]'] = 1
-            let iist: MANY = [ ]; prm['pagination[pageSize]'] = _iong
-            const pgs: number[] = _got_pages(_num, prm['pagination[page]'])
+            prm['pagination[pageSize]'] = _iong
+            let _st = (prm['pagination[page]'] - 1) * 5
+            let iist: MANY = [ ]; const pgs: number[] = _got_pages(_num, _st ? _st : 0)
             for(let i= 0; i< pgs.length; i++ ) {
                 prm['pagination[page]'] = pgs[i]
                 if (caii) { const dat: ONE | null = await caii(prm); dat ? iist.push(dat) : undefined }
-            } rej( ser_pagi_more( iist ) )
+            } rej( ser_pagi_more( iist, _num ) )
         })
     }
 }
